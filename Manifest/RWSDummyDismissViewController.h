9aa0f6a93bdcc012801a438909ffff0b7b344610
Samuel Goodwin <samuel@roundwallsoftware.com>
2014-01-27 00:52:47 +0100

    And now the Skeleton walks sorta.

---
+++ Manifest/RWSDummyDismissViewController.h
@@ -0,0 +1,13 @@
+//
+//  RWSDummyDismissViewController.h
+//  Manifest
+//
+//  Created by Samuel Goodwin on 27-01-14.
+//  Copyright (c) 2014 Roundwall Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RWSDummyDismissViewController : UIViewController
+- (IBAction)close:(id)sender;
+@end
