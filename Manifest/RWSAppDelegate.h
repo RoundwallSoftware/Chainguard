8a531cceb5cac327f616049d16741c23f554c85c
Samuel Goodwin <samuel@roundwallsoftware.com>
2014-01-26 21:04:31 +0100

    Initial Commit

---
+++ Manifest/RWSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RWSAppDelegate.h
+//  Manifest
+//
+//  Created by Samuel Goodwin on 26-01-14.
+//  Copyright (c) 2014 Roundwall Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RWSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Manifest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Manifest
+//
+//  Created by Samuel Goodwin on 26-01-14.
+//  Copyright (c) 2014 Roundwall Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RWSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RWSAppDelegate class]));
+    }
+}
