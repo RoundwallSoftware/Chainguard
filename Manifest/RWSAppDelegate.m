cd652f95300aaeb0459f6b43864628ea3b104b83
Samuel Goodwin <samuel@roundwallsoftware.com>
2014-03-01 10:10:16 +0100

    Matching gray color.

--- Manifest/RWSAppDelegate.m
+++ Manifest/RWSAppDelegate.m
@@ -28,7 +28,7 @@
 
     UIFont *headingFont = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
     [[UIBarButtonItem appearance] setTitleTextAttributes:@{ NSForegroundColorAttributeName: tintColor, NSFontAttributeName: headingFont} forState:UIControlStateNormal];
-    [[UIBarButtonItem appearance] setTitleTextAttributes:@{ NSForegroundColorAttributeName: [UIColor darkGrayColor], NSFontAttributeName: headingFont} forState:UIControlStateDisabled];
+    [[UIBarButtonItem appearance] setTitleTextAttributes:@{ NSForegroundColorAttributeName: [UIColor iOS7lightGrayColor], NSFontAttributeName: headingFont} forState:UIControlStateDisabled];
     [[UINavigationBar appearance] setBarTintColor:[UIColor colorWithWhite:1.0 alpha:0.01]];
 }
 
